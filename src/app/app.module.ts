a590aef282387accbe037f28506991291b335f86
irfan <irfan.naushad93@gmail.com>
2023-01-02 00:28:12 +0530

    front end base commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { ScorecardComponent } from './scorecard/scorecard.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
@@ -20,7 +21,7 @@ const appRoutes: Routes = [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    ScorecardComponent
+    ScorecardComponent,
   ],
   imports: [
     RouterModule.forRoot(
@@ -28,9 +29,11 @@ const appRoutes: Routes = [
       { enableTracing: true } // <-- debugging purposes only
     ),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  err: any;
+  constructor(private http: HttpClient, private router: Router) {}
 
+  ngOnInit(): void {}
+  register(form: NgForm) {
+    console.log(form.value.email);
+    const headers = { 'content-type': 'application/json' };
+    this.http
+      .post<any>(
+        'http://localhost:3000/user/login',
+        { email: form.value.email },
+        { headers }
+      )
+      .subscribe({
+        next: this.handleSuccess.bind(this),
+        error: this.handleError.bind(this),
+      });
+  }
+  handleSuccess(result: any) {
+    console.log('result :>> ', result);
+  }
+  handleError(error: any) {
+    console.log('error :>> ', error);
+  }
 }
